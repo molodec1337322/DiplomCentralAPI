6e413e2bfc28bbdce2cd08f438c694daaa19cf04
molodec1337322 <sshibanov1@yandex.ru>
2023-02-25 16:16:18 +0300

    Arduino Mega programm

---
+++ DiplomCentralAPI/Controllers/ArduinoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiplomCentralAPI.Controllers
+{
+    [Route("api/arduino")]
+    public class ArduinoController : Controller
+    {
+        public ArduinoController() { }
+
+        [HttpPost]
+        [Route("/setCommand")]
+        public IActionResult SetCommand() 
+        {
+
+            return Ok();
+        }
+
+    }
+}
