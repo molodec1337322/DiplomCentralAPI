42083d76f06f6c9e380e345e0fb2c046070947a7
molodec1337322 <sshibanov1@yandex.ru>
2023-03-19 19:32:32 +0300

    DB models update

--- DiplomCentralAPI/Controllers/ExperimentsController.cs
+++ DiplomCentralAPI/Controllers/ExperimentsController.cs
@@ -2,18 +2,27 @@
 using ArduinoUploader;
 using DiplomCentralAPI.Controllers.Utils;
 using Microsoft.AspNetCore.Mvc;
+using DiplomCentralAPI.Data.Repository.Postgres;
+using DiplomCentralAPI.Data.Interfaces;
+using DiplomCentralAPI.Data.Models;
 
 namespace DiplomCentralAPI.Controllers
 {
     [Route("api/experiments")]
     public class ExperimentsController : Controller
     {
-        public ExperimentsController() { }
+        private readonly IRepository<Schema> _schemaRepository;
+
+        public ExperimentsController(IRepository<Schema> schemaRepo) 
+        {
+            _schemaRepository = schemaRepo;
+        }
 
         [HttpGet]
         [Route("getAllList")]
         public IActionResult GetExperimentsList()
         {
+            /*
             //тут вообще должно все браться из БД, но пока затычка будет
 
             List<ExperimentData> data = new List<ExperimentData>
@@ -23,8 +32,20 @@ namespace DiplomCentralAPI.Controllers
                 new ExperimentData("Name of experement 3", "Used: 5 times"),
                 new ExperimentData("Name of experement 4", "Used: 546 times")
             };
+            */
+
+            List<ExperimentData> data = 
+
 
             return Ok(data);
+        }
+
+        [HttpGet]
+        [Route("createNewSchema")]
+        public IActionResult CreateNewSchemaExperiment()
+        {
+
+            return Ok();
         }
 
         public record ExperimentData(string Name, string Data);
