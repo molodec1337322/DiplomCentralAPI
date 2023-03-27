863ff5c0f5223f43166233266a4d8ce9f0e41956
molodec1337322 <sshibanov1@yandex.ru>
2023-03-27 19:56:59 +0300

    some fixes

--- DiplomCentralAPI/Controllers/ExperimentsController.cs
+++ DiplomCentralAPI/Controllers/ExperimentsController.cs
@@ -26,18 +26,6 @@ namespace DiplomCentralAPI.Controllers
         [Route("getAllSchemasList")]
         public IActionResult GetExperimentsList()
         {
-            /*
-            //тут вообще должно все браться из БД, но пока затычка будет
-
-            List<ExperimentData> data = new List<ExperimentData>
-            {
-                new ExperimentData("Name of experement 1", "Used: 2 times"),
-                new ExperimentData("Name of experement 2", "Used: 3 times"),
-                new ExperimentData("Name of experement 3", "Used: 5 times"),
-                new ExperimentData("Name of experement 4", "Used: 546 times")
-            };
-            */
-
             List<Schema> data = _schemaRepository.GetAll().ToList();
 
 
